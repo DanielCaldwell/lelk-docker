1da8393c31be5c8381f8ce5012f50a17928cd62b
Daniel Caldwell <daniel@danielcaldwell.com>
2015-07-07 13:30:53 -0600

    made changes to fix cert locations and rename of data generator

--- makecerts.sh
+++ makecerts.sh
@@ -1,5 +1,5 @@
 
-pushd
+pushd .
 
 if [ ! -f certs ]; then
 	mkdir certs;
@@ -10,12 +10,12 @@ cd certs
 openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout logstash-forwarder.key -out logstash-forwarder.crt -subj /CN=logstash;
 
 cd .. 
-cp -R certs ./logstash/data-container/.
-cp -R certs ./logstash_forwarder/data-container/.
+cp -R certs ./logstash/run-container/.
+cp -R certs ./logstash_forwarder/run-container/.
 
 
-ls -la ./logstash/data-container/certs/.
-ls -la ./logstash_forwarder/data-container/certs/.
+ls -la ./logstash/run-container/certs/.
+ls -la ./logstash_forwarder/run-container/certs/.
 
 popd
 
